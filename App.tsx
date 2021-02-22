f513c6cd63280bca83092a2177de61dce83b2733
hoangtjent9 <hoangtjent9@gmail.com>
2021-02-22 21:11:55 +0700

    create app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {View, Text} from 'react-native';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+const App = () => {
+  return <View><Text>new app</Text></View>
 }
+
+export default App
+
+
+
